7636b1df6daf03f0a88181cd20f8a5db034e95a8
Woich <minewoichbr@gmail.com>
2017-11-03 23:39:06 -0200

    Funfa pelo amor de deus

--- src/main/java/Pratica71.java
+++ src/main/java/Pratica71.java
@@ -19,34 +19,61 @@ public class Pratica71 {
         Scanner scan = new Scanner(System.in);
         int qtdJogadores;
         int num;
+        int pos;
+        String lixo;
         String nome;
-        String pos;
         
         System.out.println("Quantos jogadores serão?");
         
         if(scan.hasNextInt()){
             qtdJogadores = scan.nextInt();
             
-            for(int i=0; i<qtdJogadores ; i++){
-                System.out.println("Qual o número do jogador");
-                //Verifica se tem um número inteiro
-                if(scan.hasNextInt()){
-                    num = scan.nextInt();
-                    System.out.println("Qual o nome");
-                    nome = scan.nextLine();
+            for(int i=0; i<qtdJogadores; i++) {
+                System.out.println("Qual o numero");
+                    if(scan.hasNextInt()){ 
+                        num = scan.nextInt();
+                    }
+                    else{
+                        num = i+1;
+                        lixo = scan.next();
+                    }
+                    
+                    System.out.println("Qual o nome?");
+                    nome = scan.next();
                     time1.addJogador(new Jogador(num, nome));
-                }
-                else{
-                    i--;
-                    System.out.println("Tente Novamente");
-                }
             }
         }
         
         ArrayList<Jogador> listaJogadores = time1.ordena(new JogadorComparator(true, true, true));
         System.out.println(listaJogadores);
+        num = 1;
         
-   
+        while(num != 0){
+            System.out.println("Informe o numero");
+                
+                if(scan.hasNextInt())
+                    num = scan.nextInt();
+                else
+                    num = 0;
+                    
+                if(num == 0)
+                    break;
+                
+                System.out.println("Informe o nome");
+                    nome = scan.next();
+                    
+                Jogador novo = new Jogador(num, nome);
+                
+                pos = Collections.binarySearch(listaJogadores, novo);
+                
+                if(pos>=0) {
+                    listaJogadores.set(pos, novo);
+                }else{
+                    listaJogadores.add(pos*-1-1, novo);
+                }
+        }
+        
+        System.out.println(listaJogadores);
         
     }
     
